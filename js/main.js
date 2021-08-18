ebf10539058f411532ecf8076f05d610708de546
NikitaF2105 <nikitafilatov2105@gmail.com>
2021-08-18 22:16:01 +0300

    Space_tourism_portfolio_work added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $(".slider__box").slick({
+    prevArrow:
+      ' <img class="slider__arrow  slider__arrow-left" src="images/Arrow-left.svg" alt="arrow-left" />',
+    nextArrow:
+      ' <img class="slider__arrow  slider__arrow-right" src="images/Arrow-right.svg" alt="arrow-right" />',
+
+    responsive: [
+      {
+        breakpoint: 481,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $(".menu-btn").on("click", function () {
+    $(".menu__list").toggleClass("active");
+  });
+});
